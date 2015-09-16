991dde803e31379c299fb1715ff949ac4b5ce635
Nemanja Maric <nemanjammaric@gmail.com>
2015-09-16 13:39:19 +0200

    Update picture.php

--- picture.php
+++ picture.php
@@ -2,13 +2,18 @@
 
 require "Classes/Png.php";
 
-if(isset($_GET['picture']) && !isset($_GET['add'])){
-	$img = $_GET['picture'];
-	$png = new CreatePNG($img);
-	$png->Open();
-}else{
+if(isset($_GET['picture']) && isset($_GET['add'])){
 	$img = $_GET['picture'];
 	$png = new CreatePNG($img);
 	$png->Add($_GET['add'], 50, 50, 20, 13, 100, 100);
 	$png->Open();
+	
+}elseif(isset($_GET['picture']) && !isset($_GET['add'])){
+	$img = $_GET['picture'];
+	$png = new CreatePNG($img);
+	$png->Open();
+}else{
+	$img = 'image/some_image.png';
+	$png = new CreatePNG($img);
+	$png->Open();	
 }
